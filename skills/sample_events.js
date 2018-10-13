884d6d87670d504e084b2deca1b14cc86907e8bf
Glitch (dazzling-opinion) <none>
2018-10-13 13:55:54 +0000

    🚅👻 Updated with Glitch

---
+++ skills/sample_events.js
@@ -0,0 +1,21 @@
+module.exports = function(controller) {
+
+
+  controller.on('bot_space_join', function(bot, message) {
+
+    bot.reply(message, 'I am a bot, here to do your bidding.');
+
+  });
+
+  controller.on('user_space_join', function(bot, message) {
+
+    bot.reply(message, 'Hello, ' + message.raw_message.data.personDisplayName);
+
+  });
+
+  controller.hears('hello', 'direct_message,direct_mention', function(bot, message) {
+      bot.reply(message, 'Hi');
+  });
+
+  
+};
